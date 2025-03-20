48251ff0cfd78c2bc93db870d518da3063b19585
Chris <christopher.l.britton@outlook.com>
2025-03-20 18:22:16 -0400

    add patterns for getting product ids and destinations from log files for analysis - add error checking for the log scanner to avoid returning a false number of entries

--- cmd/analyzer/main.go
+++ cmd/analyzer/main.go
@@ -17,8 +17,8 @@ func main() {
 	logFilePath := "../../test/sample_logs/Core_20250319.log"
 	logEntries, err := parser.ParseLogFile(logFilePath)
 	checkError(err)
-	fmt.Printf("Found %d log entries\n", len(logEntries))
-	for i := 0; i < len(logEntries)-12000; i++ {
-		fmt.Println(logEntries[i].Timestamp.Format("2006-01-02 15:04:05"))
-	}
+	fmt.Printf("Found %d product entries\n", len(logEntries))
+	// for i := 0; i < len(logEntries)/1000; i++ {
+	// 	fmt.Println(logEntries[i].Timestamp.Format("2006-01-02 15:04:05"), logEntries[i].ProductId, logEntries[i].Destination)
+	// }
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -21,25 +21,53 @@ func ParseLogFile(filePath string) ([]log.LogEntry, error) {
 	defer logFile.Close()
 
 	timestampPattern := regexp.MustCompile(`^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`)
+	productIdPattern := regexp.MustCompile(`ASIN\s([0-9A-Z]{10})`)
+	destinationPattern := regexp.MustCompile(`to\s([\w/]+\.txt)`)
+
 	var logEntries []log.LogEntry
 
 	logScanner := bufio.NewScanner(logFile)
 	for logScanner.Scan() {
 		line := logScanner.Text()
-		timestampMatches := timestampPattern.FindStringSubmatch(line)
-		// Skip lines without a timestamp match - this isnt an error some logs could be corrupt
-		if len(timestampMatches) < 2 {
-			continue
+
+		// Element 0 is the full matched string (line from the log file) - element 1 is the captured regex match for all regex patterns
+		timeStampMatches := timestampPattern.FindStringSubmatch(line)
+		var timeStamp time.Time
+		var timeStampParsingError error
+		if len(timeStampMatches) > 1 {
+			timeStamp, timeStampParsingError = time.Parse("2006-01-02 15:04:05", timeStampMatches[1])
+			if timeStampParsingError != nil {
+				// If theres no timestamp skip the line - this is not an error some lines just dont get stamped
+				continue
+			}
 		}
 
-		// Element 0 is the full matched string - element 1 is the captured timestamp
-		timeStamp, err := time.Parse("2006-01-02 15:04:05", timestampMatches[1])
-		checkError(err)
-
-		logEntry := log.LogEntry{
-			Timestamp: timeStamp,
+		productIdMatch := productIdPattern.FindStringSubmatch(line)
+		var productId string
+		if len(productIdMatch) > 1 {
+			productId = productIdMatch[1]
 		}
-		logEntries = append(logEntries, logEntry)
+
+		destinationMatch := destinationPattern.FindStringSubmatch(line)
+		var destination string
+		if len(destinationMatch) > 1 {
+			destination = destinationMatch[1]
+		}
+
+		if !timeStamp.IsZero() && productId != "" && destination != "" {
+			logEntry := log.LogEntry{
+				Timestamp:   timeStamp,
+				ProductId:   productId,
+				Destination: destination,
+			}
+			logEntries = append(logEntries, logEntry)
+		}
 	}
+
+	// If there's an error scanning don't return log entries return nil
+	if err := logScanner.Err(); err != nil {
+		return nil, err
+	}
+
 	return logEntries, nil
 }
