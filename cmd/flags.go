24de956af5be6204f6a4920c31d4ad5218ca4e63
Chris <christopher.l.britton@outlook.com>
2025-03-25 12:17:00 -0400

    Fix apps flag usage string

--- cmd/flags.go
+++ cmd/flags.go
@@ -17,7 +17,7 @@ func CreateCLIFlags() *CLIFlags {
 	flags := &CLIFlags{}
 
 	flag.BoolVar(&flags.ShowCoreLogSummary, "core", true, "Show summary of the core log file only")
-	flag.BoolVar(&flags.ShowAllAppLogSummary, "apps", false, "Show summary of all app log files excluding core")
+	flag.BoolVar(&flags.ShowAllAppLogSummary, "apps", false, "Show summary of all app log files including core")
 	flag.BoolVar(&flags.TotalProductsOnly, "products", false, "Show only total products processed from core")
 	flag.BoolVar(&flags.BlockedProductsOnly, "blocked", false, "Show only blocked products from core")
 	flag.BoolVar(&flags.SubmittedProductsOnly, "submitted", false, "Show only submitted products from core")
